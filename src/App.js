0166dc39bad6513dc7714277761407a1fe2f3c17
aaushi <aaushi1512@gmail.com>
2023-10-25 16:06:46 +0530

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <AppHeader></AppHeader>
+      <CricketersList></CricketersList>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import CricketerDetails from './components/CricketerDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/details/:id",
+    element: <CricketerDetails />,
+  },
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={appRouter} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
